a1bb4f975a28c32e0dc8df34e669c9dd04f09a4f
Lucas Müller <lucas@muller.codes>
2023-05-18 22:52:34 -0300

    wip: first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started
+
+1. Install yarn
+```sh
+    npm install -g yarn
+```
+2. Set yarn to v3
+```sh
+    yarn set version berry
+```
+3. Install dependencies
+```sh
+    yarn
+```
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Client } from 'discordx';
+
+const client = new Client({ intents: 'Guilds' });
+
+client.on('ready', () => console.log('Ready to begin!'));
+
+client.on('disconnected', () => {
+	console.log('Disconnected!');
+	process.exit(1);
+});
+
+// This code will run once the bot receives any message.
+client.on("message", msg => {
+	if (msg.content.indexOf('ping') === 0) {
+		msg.channel.send('pong!');
+		console.log('pong-ed ' + msg.author.username);
+	}
+});
+
+client.login('BOT_TOKEN');
