d05b6b066923f414d0ba0a435a4c61d86ef5b86e
mdrgoncalves <mdr.goncalves@hotmail.com>
2024-10-18 21:31:49 -0300

    refactor: update discord.js intents and add HTTP server
    This commit refactors the code by updating the discord.js intents to include GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, and GatewayIntentBits.MessageContent. Additionally, it adds an HTTP server to the code, allowing the bot to respond to requests on the specified port.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
+import http from 'http';
 
-import ready from "./listeners/ready";
-import interactionCreate from "./listeners/interactionCreate";
-import embedMessage from "./listeners/embedMessage";
+import ready from './listeners/ready';
+import interactionCreate from './listeners/interactionCreate';
+import embedMessage from './listeners/embedMessage';
 
-console.log("Bot is starting...");
+console.log('Bot is starting...');
 dotenv.config();
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 ready(client);
@@ -21,3 +22,13 @@ interactionCreate(client);
 embedMessage(client);
 
 client.login(process.env.BOT_TOKEN);
+
+const PORT = process.env.PORT || 3000;
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running\n');
+  })
+  .listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
