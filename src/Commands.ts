c34dd50ed3721feeaf02bcfe50152eeb339ef675
mdrgoncalves <mdr.goncalves@hotmail.com>
2024-10-20 18:12:36 -0300

    refactor: update Commands.ts to include TestNews command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,7 +1,5 @@
-import { FxTweet } from "./commands/FxTweet";
+import { FxTweet } from './commands/FxTweet';
 import { Hello } from './commands/Hello';
+import { TestNews } from './commands/TestNews';
 
-export const Commands = [
-    Hello, 
-    FxTweet
-];
+export const Commands = [Hello, FxTweet, TestNews];
---
+++ src/commands/TestNews.ts
@@ -0,0 +1,29 @@
+import {
+  CommandInteraction,
+  Client,
+  ApplicationCommandType,
+  InteractionResponse,
+} from 'discord.js';
+import { embedConstructor } from '../utils/embedConstructor';
+import { fetchLatestNews } from '../services/newsService';
+
+import type { Command } from '../interfaces/Command';
+
+export const TestNews: Command = {
+  name: 'testnews',
+  description: '[DEBUG] Test return news',
+  type: ApplicationCommandType.ChatInput,
+  run: async (
+    client: Client,
+    interaction: CommandInteraction,
+  ): Promise<InteractionResponse<boolean>> => {
+    if (!(interaction.user.id !== '142075485000695809')) {
+      return interaction.reply('You are not allowed to use this command');
+    }
+
+    const news = await fetchLatestNews();
+    if (!news) return interaction.reply('No news found');
+    const embed = embedConstructor(news);
+    return interaction.reply({ embeds: [embed] });
+  },
+};
